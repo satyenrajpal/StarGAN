558130247828381754ff715b7b46708f379a786e
satyenrajpal <satyen.2.rajpal@gmail.com>
2018-06-23 19:21:33 -0400

    solver

--- solver.py
+++ solver.py
@@ -358,7 +358,7 @@ class Solver(object):
                     with torch.no_grad():
                         x_fake_list = [x_fixed]
                         for c_fixed in c_fixed_list:
-                            x_fake_list.append(self.G(x_fixed, c_fixed,step,alpha))
+                            x_fake_list.append(self.G(x_fixed, c_fixed,step,alpha)[0])
                         x_concat = torch.cat(x_fake_list, dim=3)
                         sample_path = os.path.join(self.sample_dir, '{}-{}-images.jpg'.format(step,itr+1))
                         save_image(self.denorm(x_concat.data.cpu()), sample_path, nrow=1, padding=0)
