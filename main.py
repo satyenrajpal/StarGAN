7ba7b257c43885a69ded814e56b4fb347019b4af
satyenrajpal <satyen.2.rajpal@gmail.com>
2018-07-02 09:32:20 -0400

    parser

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     parser.add_argument('--g_lr', type=float, default=0.0001, help='learning rate for G')
     parser.add_argument('--d_lr', type=float, default=0.0001, help='learning rate for D')
     parser.add_argument('--n_critic', type=int, default=1, help='number of D updates per each G update')
-    parser.add_argument('--start_steps', type=int, default=0, help='Steps to start from')
+    parser.add_argument('--start_step', type=int, default=0, help='Steps to start from')
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam optimizer')
     parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam optimizer')
     parser.add_argument('--resume_iters', type=int, default=None, help='resume training from this step')
