9197dc9ea61a3a91a48a25cd4c21de4fd067f0f0
satyenrajpal <satyen.2.rajpal@gmail.com>
2018-07-13 18:57:16 -0400

    mul

--- solver.py
+++ solver.py
@@ -14,7 +14,7 @@ class log_gaussian:
     def __call__(self,x,mu,var):
 
         logli=-0.5*(var.mul(2*np.pi)+1e-6).log() - \
-              (x-mu).pow(2).div(var.mul(2.0)+1e-6)
+              (x-mu).pow(2).div_(var.mul_(2.0)+1e-6)
 
         return logli.sum(1).mean().mul(-1)
 
