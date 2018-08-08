4f1050e189994877da824961c5edab1faf1a8e4a
satyenrajpal <satyen.2.rajpal@gmail.com>
2018-08-08 12:46:13 +0530

    int2list

--- model.py
+++ model.py
@@ -73,7 +73,7 @@ class Generator(nn.Module):
         
 class Discriminator(nn.Module):
     """Discriminator network with PatchGAN."""
-    def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
+    def __init__(self, image_size=128, conv_dim=64, c_dim=[5], repeat_num=6):
         super(Discriminator, self).__init__()
         layers = []
         layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1))
@@ -88,10 +88,11 @@ class Discriminator(nn.Module):
         kernel_size = int(image_size / np.power(2, repeat_num))
         self.main = nn.Sequential(*layers)
         self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+        self.conv2 = nn.ModuleList([nn.Conv2d(curr_dim, dim, kernel_size=kernel_size, bias=False) for dim in c_dim])
         
     def forward(self, x):
         h = self.main(x)
         out_src = self.conv1(h)
-        out_cls = self.conv2(h)
-        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+        out_cls = [conv2(h) for conv2 in self.conv2]
+        out_cls = [cls_probs.view(cls_probs.size(0), cls_probs.size(1)) for cls_probs in out_cls]
+        return out_src, out_cls
