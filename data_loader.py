752ba4c77bd7f8986068c842722b67deec0bdb88
satyenrajpal <satyen.2.rajpal@gmail.com>
2018-08-10 12:14:22 +0530

    pose

--- data_loader.py
+++ data_loader.py
@@ -3,7 +3,7 @@ from torchvision import transforms as T
 from torchvision.datasets import ImageFolder
 from PIL import Image
 import torch
-import os
+import os, sys
 import random
 import shutil, glob
 
@@ -92,24 +92,69 @@ def pre_RaFD(root):
     os.chdir(cwd)
     print("Preprocessing of RaFD done.")
 
+class RaFD_dataset(data.Dataset):
+    def __init__(self, img_dir, transform, mode='train'):
+        self.transform = transform
+        self.train_dataset = []
+        self.test_dataset=[]
+        self.mode = mode
+        self.img_dir = img_dir
+        
+        self.preprocess(img_dir)
+    
+    def preprocess(self, img_dir):
+        cwd = os.getcwd()
+        os.chdir(img_dir)
+        
+        attr = ['angry','contemptuous','disgusted','fearful','happy','neutral','sad', 'surprised']
+        angles = [0, 45, 90, 135, 180]
+        for i, filename in enumerate(glob.glob("*.jpg")):
+            split = filename.split("_")
+            angle = int(split[0][-3:])
+            expression = split[4]
+            gender = split[3]
+            race = split[2]
+            if i<100:
+                self.test_dataset.append([filename, attr.index(expression), angles.index(angle)])
+            else:
+                self.train_dataset.append([filename, attr.index(expression), angles.index(angle)])
+        os.chdir(cwd)
+
+    def __getitem__(self, index):
+        
+        dataset = self.train_dataset if self.mode=='train' else self.test_dataset
+        filename, expression_idx, angle_idx = dataset[index]
+
+        image = Image.open(os.path.join(self.img_dir, filename))
+        img_transformed = self.transform(image)
+        
+        return img_transformed, torch.FloatTensor([expression_idx]), torch.FloatTensor([angle_idx])
+
+    def __len__(self):
+        return len(self.train_dataset) if self.mode=='train' else len(self.test_dataset) 
+
 def get_loader(image_dir, attr_path, selected_attrs, crop_size=178, image_size=128, 
-               batch_size=16, dataset='CelebA', mode='train', num_workers=1):
+               batch_size=16, dataset='CelebA', mode='train', num_workers=1, pose=False):
     
     """Build and return a data loader."""
     transform = []
-    if mode == 'train':
+    if mode == 'train' and not pose:
         transform.append(T.RandomHorizontalFlip())
     transform.append(T.CenterCrop(crop_size))
     transform.append(T.Resize(image_size))
     transform.append(T.ToTensor())
     transform.append(T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
     transform = T.Compose(transform)
+    print("transform: ", transform)
 
     if dataset == 'CelebA':
         dataset = CelebA(image_dir, attr_path, selected_attrs, transform, mode)
-    elif dataset == 'RaFD':
+    elif dataset == 'RaFD' and not pose:
         dataset = ImageFolder(image_dir, transform)
-
+    elif dataset == 'RaFD' and pose:
+        print("Fetching RafD Pose dataset")
+        dataset = RaFD_dataset(image_dir, transform)
+    
     data_loader = data.DataLoader(dataset=dataset,
                                   batch_size=batch_size,
                                   shuffle=(mode=='train'),
